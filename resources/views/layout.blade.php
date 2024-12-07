f4274be2d3c7a02881531d8787d72937f21c9116
Raaz <raazkatwal5@gmail.com>
2024-12-07 20:08:47 +0545

    changed title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="icon" href={{ asset("icon.png") }}>
-    <title>Xotalink | URL Shortner</title>
+    <title>XotaLink | URL Shortner</title>
     @vite('resources/css/app.css')
 </head>
 
